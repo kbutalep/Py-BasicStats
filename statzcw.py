17ba61f61873c1b33ac61aba99cead7e45f4facc
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-17 12:28:37 -0400

    built out shell of program

---
+++ statzcw.py
@@ -0,0 +1,82 @@
+
+# You may only use the following functions to construct your code:
+#
+# python builtin sum()
+# python builtin max()
+# python builtin min()
+# python Math function Math.sqrt()
+# python normal operators on floats (*, /, +, -, etc)
+
+import math
+from typing import List
+
+
+
+def zcount(list: List[]) -> float:
+    """
+    Count of numbers in list
+    :param list:
+    :return:
+    """
+    pass
+
+def zmean(list: List[]) -> float:
+    """
+    mean = sum(data0) / count(data0)
+    :param list:
+    :return:
+    """
+    pass
+
+def zmode(list: List[]) -> float:
+    """
+    the mode is the most common value in list
+    :param list:
+    :return:
+    """
+    pass
+
+def zmedian(list: List[]) -> float:
+    """
+    The median is the middle value
+    :param list:
+    :return:
+    """
+    pass
+
+def zvariance(list: List[]) -> float:
+    """
+    Variance measures how far a data set is spread out.
+    It is mathematically defined as the average of the squared differences from the mean.
+    :param list:
+    :return:
+    """
+    pass
+
+def zstddev(list: List[]) -> float:
+    """
+    The standard deviation is just the square root of the variance of a list,
+    std_dev = square-root(variance(data0))
+    :param list:
+    :return:
+    """
+    pass
+
+def zstderr(list: List[]) -> float:
+    """
+    This is simply the standard deviation divided by the square root of the count...
+    std_err = standard_deviation(data0) / square_root( count(data0) )
+
+    :param list:
+    :return:
+    """
+    pass
+
+def zcorr(listx: List[], listy: List[]) -> float:
+    """
+    correlation is the covariance divided by the stddev of each list multiplied together
+    :param listx:
+    :param listy:
+    :return:
+    """
+    pass
